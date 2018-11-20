d7319ee6735503a7e448fe222f992f7884f98f0f
Jori Romans <jromans@ualberta.ca>
2018-11-19 23:15:44 -0700

    Fixed CareGiverCommentTest

--- app/src/test/java/com/example/healthtracker/CareGiverCommentTest.java
+++ app/src/test/java/com/example/healthtracker/CareGiverCommentTest.java
@@ -1,38 +1,46 @@
 package com.example.healthtracker;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by caochenlin on 2018/11/2.
  */
 
-/*
+
 public class CareGiverCommentTest {
     private String comment;
     private Problem parentProblem;
     private CareGiverComment careGiverComment;
     private String title;
+
     @Before
     public void setUp() {
         title = "Rash";
         comment = "Get a rash after eating some seafood";
-        parentProblem = new Problem(title, new Date(), comment);
-        careGiverComment = new CareGiverComment("Allergy", "Stay away from seafood");
+        parentProblem = new Problem("Parent", new Date(), "Description for the parent");
+        careGiverComment = new CareGiverComment(title, comment,  parentProblem);
     }
 
     @Test
     public void getComment() {
-        assertEquals(careGiverComment.getComment(),comment);
+        assertEquals(careGiverComment.getComment(), comment);
     }
 
     @Test
     public void setComment() {
         String newComment = "Rash is disappearing";
         careGiverComment.setComment(newComment);
-        assertEquals(careGiverComment.getComment(),newComment);
+        assertEquals(careGiverComment.getComment(), newComment);
     }
 
     @Test
     public void getTitle() {
-        assertEquals(careGiverComment.getTitle(),title);
+        assertEquals(careGiverComment.getTitle(), title);
 
     }
 
@@ -40,8 +48,9 @@ public class CareGiverCommentTest {
     public void setTitle() {
         String newTitle = "Rash2";
         careGiverComment.setTitle(newTitle);
-        assertEquals(careGiverComment.getTitle(),newTitle);
+        assertEquals(careGiverComment.getTitle(), newTitle);
     }
+}
 
 
-*/
+
