5679dc5b8fbb45226da88ef6f9177fc9f70ace73
tylerwatson98 <32936743+tylerwatson98@users.noreply.github.com>
2018-12-02 16:12:31 -0700

    Hard copied Mikes code into my branch

---
+++ app/src/main/java/com/example/healthtracker/EntityObjects/search_results_comments.java
@@ -0,0 +1,38 @@
+package com.example.healthtracker.EntityObjects;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.healthtracker.Contollers.UserDataController;
+import com.example.healthtracker.R;
+
+import java.util.ArrayList;
+
+public class search_results_comments extends Activity {
+
+    Object[] hits;
+    int index;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result_comments);
+
+        hits = UserDataController
+                .unserializeObjectArray(this, getIntent().getStringExtra("hits"));
+        index = getIntent().getIntExtra("problemIndex", -1);
+
+        ArrayList<Problem> problems = (ArrayList<Problem>)hits[0];
+        ArrayList<CareProviderComment> comments = problems.get(index).getcaregiverRecords();
+
+        ArrayAdapter<CareProviderComment> adapter =
+                new ArrayAdapter<CareProviderComment>(this, android.R.layout.simple_list_item_1, comments);
+
+        ListView commentList = findViewById(R.id.search_comments);
+        commentList.setAdapter(adapter);
+    }
+
+}
